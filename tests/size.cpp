ceeedcc910c32160042fb828e500f1b57e6e63bf
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-10-07 14:26:43 +0300

    Update size.cpp

--- tests/size.cpp
+++ tests/size.cpp
@@ -1,37 +1,49 @@
 #include "../include/BinarySearchTree.h"
 #include "catch.hpp"
 
-SCENARIO("size of empty tree must be equal 0") {
-    GIVEN("empty tree") {
-        BinarySearchTree<int> tree;
-        WHEN("decide size") {
-            THEN("size must be equal 0") {
-                REQUIRE(tree.size() == 0);
-            }
-        }
-    }
-}
-SCENARIO("size of non-empty tree must not be equal 0") {
-    GIVEN("non-empty tree") {
-        BinarySearchTree<int> tree{1, 3, 5};
-        WHEN("decide size") {
-            THEN("size must not be equal 0") {
-                REQUIRE(tree.size() != 0);
-            }
-        }
-    }
-}
-SCENARIO("size of constant tree must be equal to size of non-constant tree") {
-    GIVEN("constant and non-constant trees")
+#include "catch.hpp"
+#include <BinarySearchTree.hpp>
+SCENARIO ("Size of empty tree must be 0", "[size 0]")
+{
+    GIVEN ("tree is empty")
     {
-        const BinarySearchTree<int> tree1{1, 3, 5, 0};
-        BinarySearchTree<int> tree2{1, 3, 5, -1};
-        WHEN("compare sizes")
-        {
-            THEN("sizes must be equal")
-            {
-                REQUIRE(tree1.size() == tree2.size());
-            }
-        }
+        BinarySearchTree <int> tree;
+                        WHEN ("decide size")
+                        {
+                            THEN ("size must be 0")
+                            {
+                                REQUIRE(tree.size()==0);
+                            }
+                        };
+
     }
+}
+SCENARIO ("Size of not empty tree is not 0!", "[size !0]")
+{
+    GIVEN("tree is not empty")
+    {
+        BinarySearchTree <int> tree = {1};
+        WHEN("decide size")
+         {
+             THEN ("size is not 0")
+             {
+              REQUIRE(tree.size()!=0)   ;
+             }
+         }
+    }
+}
+SCENARIO("Size for const & !const ", "[const&!const]")
+{
+  GIVEN("const & non-const object")
+  {
+      BinarySearchTree<int> t1={1,2,3};
+      const BinarySearchTree<int> t2={2,3,4};
+      WHEN ("decide size")
+      {
+       THEN ("sizes must be equal")
+       {
+           REQUIRE(t1.size()==t2.size());
+       }
+      }
+  }
 }
