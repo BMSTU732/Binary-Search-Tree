a6e3deeea8f82f2e1e886a9e85b412e7ebe4080b
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-12-06 01:32:36 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,32 +1,42 @@
 #include "../include/BinarySearchTree.h"
+int main()
+{
+	BinarySearchTree<int> tree= { 8,10,3,6,7,14,13 }; 
+	//Checking "insert" function
+	tree.insert(5);
+	//Checking "find" function
+	tree.find(10);
 
-
-int main() {
-	BinarySearchTree<int> Tree{ 11, 12, 15, 9 };
-	auto a = 8;
-	Tree.Insert(a);
-
-	std::ofstream file("C:/Users/Роман/Desktop/BinarySearchTree.txt");
-
-	file << Tree;
-	file.close();
-
-	std::cin >> Tree;
-	std::cout << Tree << std::endl;
-
-	std::ifstream file2("C:/Users/Роман/Desktop/BinarySearchTree.txt");
-	BinarySearchTree<int> Tree2;
-	file2 >> Tree2;
-	std::cout << Tree2 << std::endl;
-
-	BinarySearchTree<int> Tree3(std::move(Tree));
-	std::cout << Tree3 << std::endl;
-
-	Tree = Tree3;
-	std::cout << Tree << std::endl;
+	BinarySearchTree<int> tree3;
+	//Checking ">>" operator
+	std::cin >> tree3;
+	std::ofstream file1("C:\\tree.txt");
+	//Cheking "<<" file operator
+	file1 << tree; 
+	//Cheking copy assignment operator
+	tree = tree3;
+	//Checking ">>" operator
+	std::cout << tree<<std::endl;
 	
-	std::cout << (Tree == Tree2) << std::endl;
+    auto a = tree.find(1);
+	std::cout << (*a) << std:: endl;
+	//Cheking comparison operator
+	if (tree == tree3) 
+	{
+		std::cout << "Trees are equal!" << std::endl;
+	}
+	else { std::cout << "Trees aren't equal :( " << std::endl; }
+	BinarySearchTree<int> tree2 = {1,2,3,4,5,6};
 
-	system("pause");
-	return 0;
+	std::ifstream file2("C:\\tree33.txt");
+	//Checking ">>" file operator
+	file2 >> tree2;
+	//Checking move assignment operator
+	tree2 = std::move(tree);
+	std::cout << tree2 << std::endl;
+        BinarySearchTree<int> tree4 = { 6,3,8,7,2,9};
+	if (tree4.remove(3) == true)
+	{
+		std::cout << "After removing the element: " << tree4 << std::endl;
+	}
 }
